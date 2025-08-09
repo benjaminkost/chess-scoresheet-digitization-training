a129c44dbc904539b58abdeeafa82985f6a1ec05
Ben Kostka <mail@ben-kostka.de>
2025-08-09 17:26:38 +0200

    feature-14-decouple-trainer-interface-from-data-processing: delete methods that had no direct concern of the training domain (and was more related to process data)

--- src/scripts/scripts_for_steps/trainer_module.py
+++ src/scripts/scripts_for_steps/trainer_module.py
@@ -3,8 +3,6 @@ from abc import ABC, abstractmethod
 from evaluate import load
 import mlflow
 from transformers import TrainingArguments, ProcessorMixin, VisionEncoderDecoderModel, Trainer as ML_Trainer
-
-from src.scripts.scripts_for_steps.data_module import DataModule, DataModuleTransformer
 from src.scripts.scripts_for_steps.hyperparameter_util import ModelHyperparameters
 
 # Logger definition
@@ -34,50 +32,14 @@ class Trainer(ABC, ModelHyperparameters):
         pass
 
     @abstractmethod
-    def get_dataset(self):
-        pass
-
-    @abstractmethod
-    def set_dataset(self, dataset):
-        pass
-
-    @abstractmethod
     def get_train_dataset(self):
-        pass
-
-    @abstractmethod
-    def set_train_dataset(self, train_dataset):
         pass
 
     @abstractmethod
     def get_test_dataset(self):
         pass
 
-    @abstractmethod
-    def set_test_dataset(self, test_dataset):
-        pass
-
     # business logic
-    @abstractmethod
-    def ingest_data(self, datasource):
-        pass
-
-    @abstractmethod
-    def preprocess_dataset(self):
-        pass
-
-    @abstractmethod
-    def encode_labels(self, split, feature_column, target_column):
-        pass
-
-    @abstractmethod
-    def split_train_test(self, split, feature_column, target_column):
-        pass
-
-    @abstractmethod
-    def convert_feature_label_lists_into_dict(self, feature_list: list, label_list: list, feature_name: str, label_name: str):
-        pass
-
     @abstractmethod
     def train(self, run_name: str, experiment_name:str, model_flavor="pytorch", tags=None):
         pass
@@ -87,13 +49,11 @@ class TransformerTrainer(Trainer, ABC):
     @abstractmethod
     def __init__(self,
                  model,
-                 data_module: DataModuleTransformer,
                  processor,
                  trainer=None,
                  training_args=None
                  ):
         self._model = model
-        self._data_module = data_module
         self._processor = processor
         self._trainer = trainer
         self._training_args = training_args
@@ -117,9 +77,6 @@ class TransformerTrainer(Trainer, ABC):
         pass
 
     # business logic
-    def encode_dataset(self, split, feature_column, target_column):
-        pass
-
     def compute_metrics(self, pred):
         """
         Compute metrics based on the prediction result.
@@ -133,7 +90,6 @@ class CNNTransformerTrainer(TransformerTrainer):
 
     def __init__(self,
                  model: VisionEncoderDecoderModel,
-                 data_module: DataModuleTransformer,
                  processor: ProcessorMixin,
                  trainer: ML_Trainer,
                  training_args: TrainingArguments,
@@ -145,7 +101,6 @@ class CNNTransformerTrainer(TransformerTrainer):
                  ):
         # Save processing classes
         self._model = model
-        self._data_module = data_module
         self._processor = processor
         self._trainer = trainer
 
@@ -180,97 +135,14 @@ class CNNTransformerTrainer(TransformerTrainer):
     def get_processor(self):
         return self._processor
 
-    def get_dataset(self):
-        return self._dataset
-
-    def set_dataset(self, dataset):
-        self._dataset = dataset
-
     def get_train_dataset(self):
         return self._train_dataset
-
-    def set_train_dataset(self, train_dataset):
-        self._train_dataset = train_dataset
 
     def get_test_dataset(self):
         return self._test_dataset
 
-    def set_test_dataset(self, test_dataset):
-        self._test_dataset = test_dataset
-
     def get_model(self):
         return self._model
-
-    def ingest_data(self, datasource):
-        """
-        fetching a dataset from huggingface and initializing the _dataset attributes
-
-        :param datasource: huggingface uri
-        """
-
-        hf_owner = datasource.split("/")[0]
-        hf_dataset_name = datasource.split("/")[1]
-
-        dataset = self._data_module.ingest_data(hf_owner, hf_dataset_name)
-        self.set_dataset(dataset)
-
-        logger.info(f"Dataset attribute initialized with huggingface Dataset: {hf_owner}/{hf_dataset_name}")
-
-    def preprocess_dataset(self):
-        """
-        Preprocess the dataset so that the images are changed as needed for training.
-        Then the methods reassigns the _dataset attributes.
-        """
-
-        dataset = self._data_module.preprocess_dataset(self.get_dataset())
-
-        self.set_dataset(dataset)
-
-        logger.info(f"Dataset was preprocessed")
-
-    def encode_dataset(self, split, feature_column, target_column):
-        """
-        Encode the labels so that the labels are correctly formatted for training.
-        This reassigns the _dataset attribute.
-
-        """
-        encoded_dataset = self._data_module.encode_dataset(self.get_dataset(), split, feature_column, target_column)
-
-        self.set_dataset(encoded_dataset)
-
-        logger.info(f"Dataset was encoded")
-
-    def split_train_test(self, split, feature_column, target_column):
-        """
-        Split the dataset into train and test sets.
-
-        """
-        try:
-            X_train, X_test, y_train, y_test = self._data_module.split_data(self.get_dataset(), split, feature_column, target_column)
-
-            train_dataset = self.convert_feature_label_lists_into_dict(X_train, y_train, feature_column, target_column)
-            test_dataset = self.convert_feature_label_lists_into_dict(X_test, y_test, feature_column, target_column)
-
-            self.set_train_dataset(train_dataset)
-            self.set_test_dataset(test_dataset)
-        except Exception as e:
-            logger.error(f"Dataset was not found, cannot split into train and test sets. Error: {e}")
-
-    def convert_feature_label_lists_into_dict(self, feature_list: list, label_list: list, feature_name: str, label_name: str):
-        """
-        Convert the feature list and the corresponding label list into a dictionary.
-
-        :param feature_list: list of pixel values per image
-        :param label_list: list of tokens for each label
-
-        """
-        res_dataset = {}
-
-        for feature, label in zip(feature_list, label_list):
-            res_dataset[feature_name] = feature
-            res_dataset[label_name] = label
-
-        return res_dataset
 
     def train(self, run_name: str, experiment_name:str, model_flavor="pytorch", tags=None):
         logger.info(f"MLflow uri is: {mlflow.get_tracking_uri()}")
